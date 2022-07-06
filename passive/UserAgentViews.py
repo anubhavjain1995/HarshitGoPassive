39a5b6fd650aab1a8052512d530dce5922a8429b
anubhav <anubhav@frenzi.in>
2022-07-07 01:14:02 +0530

    user_agent_curd

--- passive/UserAgentViews.py
+++ passive/UserAgentViews.py
@@ -37,3 +37,59 @@ class UserAgentViews(viewsets.ModelViewSet):
                     'message': 'Error',
                     'data': str(e)
                 })
+
+    @action(detail=False, methods=['post'])
+    def update_user_agent(self, request):
+        data = request.data
+        try:
+            if not data.get('uuid'):
+                return Response({
+                    'status': consts.Error,
+                    'message': 'uuid is required'
+                })
+            obj = UserTable.objects.get(uuid=data.get('uuid'))
+            serializer = UserSerializer(obj, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status': consts.Success,
+                    'message': 'User Updated Successfully',
+                    'data': serializer.data
+                })
+            return Response({
+                'status': consts.Error,
+                'message': 'Error',
+                'error': serializer.errors
+            })
+        except Exception as e:
+            return Response({
+                'status': consts.Error,
+                'message': 'Error',
+                'error': type(e)
+            })
+
+    @action(detail=False, methods=['post'])
+    def get_user_agent_detail(self, request):
+        try:
+            data = request.data
+            # pdb.set_trace()
+            if not data.get('uuid'):
+                return Response({
+                    'status': consts.Error,
+                    'message': 'uuid is required'
+                })
+
+            user = UserTable.objects.get(uuid=data.get('uuid'))
+            serializer = UserSerializer(user)
+
+            return Response({
+                'status': consts.Success,
+                'message': 'User retrived Successfully',
+                'data': serializer.data
+            })
+        except Exception as e:
+            return Response({
+                'status': consts.Error,
+                'message': 'Error',
+                'error': str(e)
+            })
